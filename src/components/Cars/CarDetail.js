69e69b5136d755ea8b9b55036ba295600e58c058
Andres Chaves <andres1009_93@hotmail.com>
2018-08-24 05:00:31 -0600

    Added some more information to the CarDetailComponent.

--- src/components/Cars/CarDetail.js
+++ src/components/Cars/CarDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, PageHeader, Image } from 'react-bootstrap';
+import { Grid, PageHeader, Image, Table, Panel } from 'react-bootstrap';
 import { connect } from 'react-redux';
 
 const CarDetail = (props) => (
@@ -8,16 +8,39 @@ const CarDetail = (props) => (
       <PageHeader>
         {props.car.brand} {props.car.model} ({props.car.year})
       </PageHeader>
+      <p>{props.car.description}</p>
+      <Panel>
+        <Panel.Heading>
+          Mas información
+        </Panel.Heading>
+        <Panel.Body>
+          <Table>
+            <tbody>
+              <tr>
+                <td>Transmisión:</td>
+                <td>{props.car.transmission}</td>
+              </tr>
+
+              <tr>
+                <td>Combustible:</td>
+                <td>{props.car.fuel}</td>
+              </tr>
+
+              <tr>
+                <td>Kilometraje:</td>
+                <td>{props.car.mileage}</td>
+              </tr>
+            </tbody>
+          </Table>
+        </Panel.Body>
+      </Panel>
       <Image className="car-detail-image" src={props.car.img} />
     </div>
   </Grid>
 );
 
-const mapStateToProps = (state, ownProps) => {
-  console.log(state);
-  return {
+const mapStateToProps = (state, ownProps) => ({
   car: state.find((car) => ownProps.match.params.id === car.id)
-  }
-};
+});
 
 export default connect(mapStateToProps)(CarDetail);
